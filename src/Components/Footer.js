5ff3d981f99732e31453d4e58c2377d5719d39c1
Vivek Sheladiya <vivekheladiya32@gmail.com>
2023-04-13 12:42:41 +0530

    Updated

---
+++ src/Components/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <div>
+        <footer className="bg-dark text-light py-3">
+          <p className="text-center">Copyright &copy; TodosList.com</p>
+        </footer>
+      </div>
+    );
+  }
+}
