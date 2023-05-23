4f2c57151b8fb1327819ff42570e2aabaadd17eb
Vivek Sheladiya <vivekheladiya32@gmail.com>
2023-05-23 12:19:43 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import "./App.css";
-import About from "./Components/About";
-import Footer from "./Components/Footer";
 import Home from "./Components/Home";
+import { Login } from "./Components/Login";
 import Navbar from "./Components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Signup } from "./Components/Signup";
 
 function App() {
   return (
@@ -12,10 +12,10 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
       </Router>
-      <Footer />
     </>
   );
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -22,14 +22,14 @@ export default function Navbar() {
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-              <Link className="nav-link" to="/">
-                Home
+            <li className="nav-item">
+              <Link className="nav-link" to="/login">
+                Login
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/about">
-                About
+              <Link className="nav-link" to="/signup">
+                Signup
               </Link>
             </li>
           </ul>
